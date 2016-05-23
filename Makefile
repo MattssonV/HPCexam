965cb3ac8c5884b53b5d0d5501f06c61909e2abb
MattssonV <Viktor_m92@hotmail.com>
2016-05-23 17:20:11 +0200

    create_random_array works

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 
 exam: main.c
-	$(CC) -o exam main.c
+	$(CC) -o exam main.c -I /*.h
 
 clean:
 	rm exam
--- main.c
+++ main.c
@@ -14,10 +14,60 @@
 #include <time.h>
 #include "funcs.h"
 
+int randChar();
+
 void printtime(clock_t s, clock_t e)
 {
   printf("time: %f\n", (double)(e-s)/CLOCKS_PER_SEC);    
 }
+
+void create_random_array(star_t * stars, int size)
+{
+    srand(time(NULL));
+    int i;
+    for (i = 0; i<size; i++) {
+        stars[i].index = 0;
+        stars[i].spectralType = (char) randChar();
+        stars[i].subType = (unsigned short) rand() % 10;
+        sprintf(stars[i].designation, "%c%d.%d", stars[i].spectralType, stars[i].subType, stars[i].index);
+        stars[i].position.x = rand()/RAND_MAX * 1e5 - 5e4;
+        stars[i].position.y = rand()/RAND_MAX * 1e5 - 5e4;
+        stars[i].position.z = rand()/RAND_MAX * 3e3 - 1.5e3;
+        stars[i].magnitude = rand()/RAND_MAX*30 - 10;
+    }
+}
+
+int randChar(){
+    int a = rand() % 9;
+    if (a == 0)
+        return 79; //O
+    else if (a == 1)
+        return 66; //B
+    else if (a == 2)
+        return 65; //A
+    else if (a == 3)
+        return 70; //F
+    else if (a == 4)
+        return 71; //G
+    else if (a == 5)
+        return 75; //K
+    else if (a == 6)
+        return 77; //M
+    else if (a == 7)
+        return 76; //L
+    else
+        return 84; //T
+}
+
+void print_stars(star_t* array, int n)
+{
+    int i;
+    printf("\nprint_stars, n = %d:\n", n);
+    for(i = 0; i<n; i++)
+        printf("%s",array[i].designation);
+    printf("\n");
+}
+
 
 int main(int argc, char **argv)
 {
@@ -36,12 +86,13 @@ int main(int argc, char **argv)
   start = clock();
    
   create_random_array(stars, N);
-   
+
   end = clock();
   printtime(start, end);
-  // print_stars(stars, N);
+  print_stars(stars, N);
          
   printf("sorting stars:    \t");
+    /*
   start = clock();
 
   sort(stars, N);
@@ -73,4 +124,6 @@ int main(int argc, char **argv)
   printtime(start, end);
 
   display_histogram(histogram, histparams);
+     */
+    return 0;
 }
